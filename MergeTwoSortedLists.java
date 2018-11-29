04788de3d446bfb46a207a44bb0fe043d192c108
John <jebillingham3@knights.ucf.edu>
2018-11-29 14:12:53 -0500

    Fixed errors in MergeTwoSortedLists

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -11,21 +11,21 @@ class MergeTwoSortedLists
 		}
 	}
 
-	public void mergeRecur(ListNode l1, Listnode l2, ListNode sol)
+	public void mergeRecur(ListNode l1, ListNode l2, ListNode sol)
 	{
 		if (l1 == null && l2 == null)
 			return;
 
 		if (l1 == null)
 		{
-			sol.next = new Listnode(l2.val);
+			sol.next = new ListNode(l2.val);
 			mergeRecur(l1, l2.next, sol.next);
 		}
 		else if (l2 == null)
 		{
 			if (l1.val < l2.val)
 			{
-				sol.next = new Listnode(l1.val);
+				sol.next = new ListNode(l1.val);
 				mergeRecur(l1.next, l2, sol.next);
 			}
 			else
@@ -36,21 +36,23 @@ class MergeTwoSortedLists
 		}
 	}
 
-	public Listnode mergeTwoLists(ListNode l1, ListNode l2)
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2)
 	{
 		if (l1 == null && l2 == null)
 			return null;
 
+		ListNode sol;
+
 		if (l1 == null)
 		{
-			sol = new Listnode(l2.val);
+			sol = new ListNode(l2.val);
 			mergeRecur(l1, l2.next, sol);
 		}
 		else if (l2 == null)
 		{
 			if (l1.val < l2.val)
 			{
-				sol = new Listnode(l1.val);
+				sol = new ListNode(l1.val);
 				mergeRecur(l1.next, l2, sol);
 			}
 			else
