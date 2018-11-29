dd588adb68448131c9b3f42a711bbb28fe0f7011
John <jebillingham3@knights.ucf.edu>
2018-11-29 15:07:17 -0500

    Added iterative solution to ReverseList

--- ReverseList.java
+++ ReverseList.java
@@ -2,8 +2,42 @@ import java.util.*;
 
 public ReverseList
 {
+	public class ListNode
+	{
+		int val;
+		ListNode next;
+		public ListNode(int val)
+		{
+			this.val = val;
+		}
+	}
+
 	public ListNode reverseListIterative(ListNode head)
 	{
-        Deque
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+				ListNode temp = head;
+				ListNode sol;
+
+				while (temp != null)
+				{
+					stack.addFirst(temp.val);
+					temp = temp.next;
+				}
+
+				// Initialize new list.
+				if (stack.size() > 0)
+				{
+					sol = new ListNode(stack.removeFirst());
+				}
+
+				temp = sol;
+
+				while (stack.size() > 0)
+				{
+					temp.next = new ListNode(stack.removeFirst());
+					temp = temp.next;
+				}
+
+				return sol;
 	}
 }
