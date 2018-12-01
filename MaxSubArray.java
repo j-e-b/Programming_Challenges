989c5d8cec4e381ba2772f92902e4079ddaf8d64
j-e-b <jebillingham3@knights.ucf.edu>
2018-11-30 20:04:42 -0500

    Finished MaxSubArray

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,3 +1,14 @@
+/*
+Given an integer array nums, find the contiguous subarray (containing at least
+one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+*/
+
 class MaxSubArray
 {
 	public int maxSubArray(int[] nums)
