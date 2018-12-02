49e92878de69d3e6c6faacb7c8a7e42f57a02539
j-e-b <jebillingham3@knights.ucf.edu>
2018-12-01 19:00:54 -0500

    Added MaxConsecutiveOnes.java

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class MaxConsecutiveOnes
+{
+	public int findMaxConsecutiveOnes(int[] nums)
+	{
+		int count = 0, maxCount = 0;
+
+		for (int i = 0; i < nums.length; i++)
+		{
+			if (nums[i] == 1)
+			{
+				count++;
+				maxCount = Math.max(count, maxCount);
+			}
+			else
+			{
+				count = 0;
+			}
+		}
+
+		return maxCount;
+	}
+}
