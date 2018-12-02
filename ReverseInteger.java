31329024f1d5ca657ac20e3c5b947865ae426d0e
John <jebillingham3@knights.ucf.edu>
2018-12-02 14:01:36 -0500

    Solution added to ReverseInteger

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -2,5 +2,27 @@ import java.util.*;
 
 class ReverseInteger
 {
-	
+	public int reverse(int x)
+	{
+		String s = Integer.toString(x);
+		String sol = "";
+
+		if (x < 0)
+			sol += "-";
+
+		for (int i = s.length() - 1; i >= 0; i--)
+		{
+			if (x < 0 && i == 0)
+				break;
+
+			sol += s.charAt(i);
+		}
+
+		long retVal = Long.parseLong(sol);
+
+		if (retVal > Integer.MAX_VALUE || retVal < Integer.MIN_VALUE)
+			return 0;
+
+		return (int)retVal;
+	}
 }
