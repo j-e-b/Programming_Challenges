b5793c40a427aa5540da8dc8e5ee25ae38eeb94a
John <jebillingham3@knights.ucf.edu>
2019-01-24 13:35:33 -0500

    f

--- Kattis/aboveaverage.java
+++ Kattis/aboveaverage.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+/*
+https://open.kattis.com/problems/aboveaverage
+*/
+
 class aboveaverage
 {
 	public static void main(String[] args)
@@ -36,17 +40,4 @@ class aboveaverage
 			System.out.printf("%.3f%%\n", count/numStudents * 100);
 		}
 	}
-
-	/*
-	5
-	5 50 50 70 80 100
-	7 100 95 90 80 70 60 50
-	3 70 90 80
-	3 70 90 81
-	9 100 99 98 97 96 95 94 93 91
-
-	1
-	5 50 50 70 80 100
-
-	*/
 }
